fe5c54a7c0aaf7cc09f0a9453eaa43c160b110b9
bennfsx <bennfsx@gmail.com>
2024-07-04 00:20:26 +0800

    first push

---
+++ src/main/java/com/example/sso_authorization_app/SsoAuthorizationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sso_authorization_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoAuthorizationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoAuthorizationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sso_authorization_app/SsoAuthorizationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sso_authorization_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoAuthorizationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
